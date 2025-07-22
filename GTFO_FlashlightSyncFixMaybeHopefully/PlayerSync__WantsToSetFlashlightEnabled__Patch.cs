6ad706d0b558fdb02fa2d09403aee249267feb29
AuriRex <xaurirexx@gmail.com>
2025-07-22 13:59:13 +0200

    Fix silly recursion issue with EEC installed (idk)

--- GTFO_FlashlightSyncFixMaybeHopefully/PlayerSync__WantsToSetFlashlightEnabled__Patch.cs
+++ GTFO_FlashlightSyncFixMaybeHopefully/PlayerSync__WantsToSetFlashlightEnabled__Patch.cs
@@ -1,21 +1,32 @@
 using HarmonyLib;
 using Player;
 using SNetwork;
+using UnityEngine;
 
 namespace FlashlightSyncFixMaybeHopefully;
 
 [HarmonyPatch(typeof(PlayerSync), nameof(PlayerSync.WantsToSetFlashlightEnabled))]
+[HarmonyPriority(Priority.Last)]
 public class PlayerSync__WantsToSetFlashlightEnabled__Patch
 {
+    private static int _frameCount;
     public static bool Prefix(PlayerSync __instance, bool enable, bool broadcastOnly)
     {
+        var currentFrameCount = Time.frameCount;
+
+        // Anti recursion jank for EEC
+        if (_frameCount == currentFrameCount)
+            return false;
+        
+        _frameCount = currentFrameCount;
+        
         var data = new pInventoryStatus
         {
             wieldedSlot = __instance.m_agent.Inventory.WieldedSlot,
             toolEnabled = enable
         };
         
-        __instance.m_inventoryStatusPacket.Send(data, SNet_ChannelType.GameOrderCritical);
+        __instance.m_inventoryStatusPacket.Send(data, SNet_ChannelType.GameOrderCritical, SNet_SendQuality.Reliable);
         if (!broadcastOnly)
         {
             __instance.SyncInventoryStatus(data);
